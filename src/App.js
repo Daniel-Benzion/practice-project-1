1164157ceda485cd754be487da6c555242047ebf
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-22 21:20:52 -0500

    Added button and input state management

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import AddUser from './components/Users/AddUser';
 
 function App() {
+
+  const addUserHandler = (enteredUserData) => {
+
+    const userData = {
+      ...enteredUserData,
+      id: Math.random.toString()
+    }
+    //setUsers(userData)
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onSaveUserData={addUserHandler}/>
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 
-const AddUser = () => {
+const AddUser = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredAge, setEnteredAge] = useState(-1);
+
+
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  }
+
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    const userData = {
+      name: enteredName,
+      age: enteredAge
+    }
+
+    props.onSaveUserData(userData);
+    setEnteredName('');
+    setEnteredAge(-1);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" onChange={nameChangeHandler}></input>
 
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
 
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
