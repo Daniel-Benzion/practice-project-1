5b628b9ad9b4b6e6587c37333ddd0b6f6ef147cb
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-23 11:32:55 -0500

    Error modal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './ErrorModal.module.css';
+import Card from '.Card';
+import Button from './Button';
+
+const ErrorModal = (props) => {
+
+  return (
+    <Card>
+      <header>
+        <h2></h2>
+      </header>
+      <div>
+        <p></p>
+      </div>
+      <footer>
+        <Button></Button>
+      </footer>
+
+    </Card>
+  );
+}
+
+export default ErrorModal;
